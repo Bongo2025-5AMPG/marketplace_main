e388f7b4cf42fdc10846c9ce8daf8730416c3291
Bongo2025-5AMPG <ezequiel.zazueta08@gmail.com>
2025-11-06 12:03:15 -0800

    el proyecto marketplace_main con la aplicacion store

---
+++ store/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from.models import Item, Category
+from django.shortcuts import get_object_or_404
+
+
+# Create your views here.
+def home(request):
+    Items = Item.objects.filter(is_solid=False)
+    Categories = Category.objects.all()
+    context={
+        'items': Items,
+        'categories': Categories
+    }
+    return render(request,'store/home.html', context)
+
+def contact(request):
+    context = {
+        'msg':'Quieres otros productos contactame!'
+
+    }
+
+    return render (request, 'store/contact.html', context)
+
+
+def detail(request):
+    item = get_object_or_484(Item, pk=pk)
+    related_items = Item.objects.filter(category=item.category, is_solid=False).exclude(pk=pk)[0:3]
+
+    context={
+        'item' : item,
+        'related_items' : related_items
+    }
+
+
+    return redner(request, 'store/item.html', context)
